1b8e04037a2d6f506783a22bc4b0d737b4a899ac
bert <bert@example.com>
2025-07-11 17:46:32 +0800

    fix: add relative path configuration in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // 使用相对路径
   server: {
     port: 18080,
     host: true
